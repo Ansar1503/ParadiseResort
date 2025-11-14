3b4cb8ccf432cc9a5b0730f663bb017ee15f877d
Ansar1503 <muhammedansarma1503@gmail.com>
2025-11-14 19:52:02 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import AdminBookings from "./pages/AdminBookings";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <TooltipProvider>
+    <Sonner />
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/admin" element={<AdminBookings />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
+);
+
+export default App;
