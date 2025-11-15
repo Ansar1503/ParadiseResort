6c3863deb48179a2107d1d7373a32b7459826725
Ansar1503 <muhammedansarma1503@gmail.com>
2025-11-15 13:35:18 +0530

    feat: enhance CORS configuration to allow dynamic frontend URL and add new validation messages for booking dates and times

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,10 +8,15 @@ import { connectDB } from "./config/db";
 
 export const app = express();
 
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [frontendUrl],
+  })
+);
 app.use(express.json());
 
 app.use(Routes.BOOKINGS, bookingRouter);
--- backend/src/constants/Messages.ts
+++ backend/src/constants/Messages.ts
@@ -4,11 +4,18 @@ export const Messages = {
     FETCHED: "Bookings retrieved successfully.",
     NOT_FOUND: "Booking not found.",
   },
-
-  VALIDATION: {
-    FAILED: "Validation failed. Please check your input.",
+  DATE: {
+    INVALID_CHECKIN_DATE: "Check-in date is invalid.",
+    INVALID_CHECKOUT_DATE: "Check-out date is invalid.",
+    CHECKIN_BEFORE_TODAY: "Checkin date is in the past",
+    CHECKOUT_BEFORE_TODAY: "Checking date is in the past",
+    CHECKOUT_AFTER_CHECKIN: "Check-out must be after the check-in date.",
+    SAME_DATE_NOT_ALLOWED: "Check-in and check-out cannot be the same.",
   },
-
+  TIME: {
+    INVALID_CHECKIN_TIME: "Check-in time is invalid.",
+    INVALID_CHECKOUT_TIME: "Check-out time is invalid.",
+  },
   SERVER: {
     ERROR: "Something went wrong on the server.",
   },
