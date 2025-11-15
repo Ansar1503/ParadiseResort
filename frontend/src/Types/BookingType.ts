55d91743ea7c005c8809b824696ef5e59e11953c
Ansar1503 <muhammedansarma1503@gmail.com>
2025-11-15 10:28:30 +0530

    feat: update Booking interface to include separate check-in and check-out date and time fields; add bookingForm interface for form submission

--- frontend/src/Types/BookingType.ts
+++ frontend/src/Types/BookingType.ts
@@ -3,9 +3,23 @@ export interface Booking {
   name: string;
   email: string;
   phone: string;
-  checkIn: string;
-  checkOut: string;
+  checkInDate: string;
+  checkInTime: string;
+  checkOutDate: string;
+  checkOutTime: string;
   guests: number;
   message?: string;
   createdAt?: string;
+}
+
+export interface bookingForm {
+  name: string;
+  email: string;
+  phone: string;
+  checkInDate: string;
+  checkInTime: string;
+  checkOutDate: string;
+  checkOutTime: string;
+  guests: number;
+  message: string;
 }
